9440260b8685b12b58f12e3e2c86ab61dc6dd8be
Arihant Jain <141554163+im-Arihant-jain@users.noreply.github.com>
2023-12-15 22:00:23 +0530

    NEWcomm

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import Navbar from './Navbar.jsx';
 import { Element } from 'react-scroll';
 import About from './About.jsx';
-import { HashRouter as  Route } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Content from './Content.jsx';
 import Challenge from './Challenge.jsx';
 import Image1 from './Purple and Orange Facts YouTube Thumbnail (3).png'
@@ -18,33 +18,33 @@ ReactDOM.createRoot(document.getElementById('root')).render(
          <Navbar/>
       
          <App />
-         <Route path="/about">
+         <HashRouter path="/about">
           <Element name="about">
             <About />
           </Element>
-        </Route>  
+        </HashRouter>  
         <div className='content-creation'>  
-        <Route path="/Content">
+        <HashRouter path="/Content">
           <Element name="Content">
             <Content img ={Image3} title="DSA CONCEPTS"/>
           </Element>
-        </Route>
-        <Route path="/Content">
+        </HashRouter>
+        <HashRouter path="/Content">
           <Element name="Content">
             <Content img ={Image1} title="DSA CHALLENGE"/>
           </Element>
-        </Route>
-        <Route path="/Content">
+        </HashRouter>
+        <HashRouter path="/Content">
           <Element name="Content">
             <Content img ={Image2} title="DSA PYQS"/>
           </Element>
-        </Route>
+        </HashRouter>
         </div>
-        <Route path="/Challenge">
+        <HashRouter path="/Challenge">
           <Element name="Challenge">
             <Challenge />
           </Element>
-        </Route>
+        </HashRouter>
 
   </React.StrictMode>
 )
